eeb4fc97958d2fb20dc489a5c4da11f9f08222c1
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2022-08-21 09:59:04 +0000

    changes

--- strlen.c
+++ strlen.c
@@ -3,15 +3,28 @@
 
 
 unsigned long custom_strlen(int* arr){
+	
+	int len = 0;
+
+	__m256i mask = _mm256_setr_epi32(-1, -1, -1, -1, -1, -1, -1, -1);
+
+	__m256i result = _mm256_maskload_epi32(arr, mask);
+
+	int* res = (int*) &result;
+	for(unsigned short l = 0 ; l < 8 ; l ++){
+		printf("%d ", res[l]);
+	}
+	printf("\n");
 
 	return 0;
 }
 
 int main(int agc, char* argv[]){
 	
-	int arr[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	int arr16[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	int arr5[5] = {0, 1, 2, 3, 4};
 
-	custom_strlen(arr);
+	custom_strlen(arr16);
 
 	return 0;
 }
---
+++ testing.c
@@ -0,0 +1,10 @@
+#include<immintrin.h>
+#include<stdio.h>
+
+int * vector_add( int * first, int * second){
+}
+
+int main(int agc, char* argv[]){
+	
+	return 0;
+}
